060ed29421ed1dcdf5189b14d818fa0a71e1da16
GaryRidgway <gary-ridgway@uiowa.edu>
2020-02-04 22:20:15 -0600

    adding array entries

--- js/variables/mutations.js
+++ js/variables/mutations.js
@@ -8,17 +8,17 @@ var mutations = [
   [
     "Avian Ancestry",
     "Avaian features, such as feathers, may present themselves on mutated wyverns. They can range from full coverings to appearing only along joints and any amount in between. They vary in size, shape, and color, and physiology, so each avian mutation is uniquely different. These mutations result in lighter overall body structures, granting a more nimble frame that becomes more brittle, but much more agile in the process.",
-    "This wyvern -1 Constitution, +1 Dexterity, +10 Movement Speed"
+    "This wyvern -1 Constitution, +1 Dexterity, +10 Flying Speed"
   ],
   [
     "Delta Wings",
-    "Variations in wing styles reflect the amount of drag and stability imposed on the wyvern's aerobatic features. This wyvern has Delta Wings with folds that carry down each side of the spine and through the tail, allowing them to be more stable in flight at higher speeds.",
-    "This wyvern +1 Dexterity, +10 Movement Speed"
+    "Variations in wing styles reflect the amount of drag and stability imposed on the wyverns aerobatic features. This wyvern has Delta Wings with folds that carry down each side of the spine and through the tail, allowing them to be more stable in flight at higher speeds.",
+    "This wyvern has +1 Dexterity and +10 Flying Speed"
   ],
   [
     "Swept Wings",
-    "Variations in wing styles reflect the amount of drag and stability imposed on the wyvern's aerobatic features. This wyvern has Swept Wings, granting them a smaller set of stabilizer wings on their back legs, allowing them to be more agile in flight.",
-    "This wyvern +1 AC"
+    "Variations in wing styles reflect the amount of drag and stability imposed on the wyverns aerobatic features. This wyvern has Swept Wings, granting them a smaller set of stabilizer wings on their back legs, allowing them to be more agile in flight.",
+    "This wyvern has +1 Dexterity and +1 AC"
   ],
   [
     "Adaptive Carapace",
@@ -28,15 +28,26 @@ var mutations = [
   [
     "Adherent Grasp",
     "Increased muscle strength and differeng talon tensile strengths can give a wyvern great ability to scale sheer faces, and even move up entire mountain stealthily by holding on to their faces without being detected.",
-    "This wyvern gains proficiency in Stealth, and a Climbing Speed equivalent to it's Walking Speed"
-  ],[
-    "Titanous Form",
-    "Increased metabolisim and muscle density in their youth can cause some wyverns to reach a titanous size by adulthood, often growing to twice as large as a normal wyvern. This, while it makes them slower in flight and less aerobatic, gives them incredible strength and an incredibly fortified constitution.",
-    "Benefits"
+    "This wyvern gains proficiency in Stealth, and a Climbing Speed equivalent to its Walking Speed"
   ],
   [
-    "Title",
-    "Description of the mutation as it physically manifests.",
-    "Benefits"
+    "Titanous Form",
+    "Increased metabolisim and muscle density in their youth can cause some wyverns to reach a titanous size by adulthood, often growing up to twice as large as a normal wyvern. This, while it makes them proportionally slower in flight and less aerobatic compared to their smaller kin, gives them incredible strength and an astoundingly fortified constitution.",
+    "This wyvern doubles in size and now has the Size Category of 'Huge'. Its AC is decreased by 4, and its Dexterity is decreased by 3. Additionally, its Strength, Maximum Strength, Constitution, and Maximum Constitution increase by 3."
+  ],
+  [
+    "Acquatic Adaptation",
+    "Some mutations can completely change the natural homes of a wyvern. If bestowed with an affinity for the sea, a wyvern will grow a set of Gills, grow a Dorsal Fin, and grow a Caudal Fin on its tail similar to that of a Shark.",
+    "This wyvern gains a Swimming Speed of 60 feet, and it can breathe Air and Water."
+  ],
+  [
+    "Armor of Pangol",
+    "Akin to the well defended Pangolin, a wyverns scales might grow larger on their body, creating a natural armor plating often paired with a wider and equally armored tail. While slowed down considerably, these wyverns make up for their lessened aerobatic accuity with one of the most robust carapaces observed in nature.",
+    "This wyverns Flying speed is reduced by 30 feet, and its AC is increaded by 3"
+  ],
+  [
+    "Glauceal Wings",
+    "",
+    ""
   ],
 ]
