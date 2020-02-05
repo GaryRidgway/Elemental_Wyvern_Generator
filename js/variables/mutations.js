4558efc6c91c0d1f486dbf3994659221e0fb546d
GaryRidgway <gary-ridgway@uiowa.edu>
2020-02-04 21:29:54 -0600

    adding array entries

---
+++ js/variables/mutations.js
@@ -0,0 +1,42 @@
+// Each mutation has a name, description, and effect.
+var mutations = [
+  [
+    "Occular Accuity",
+    "This wyvern has grown four eyes in straight lines on each side of its head to replace the two a wyvern would normally have. These eight eyes have no pupils to be seen, and are instead one solid color of your choosing. These eyes, while useful for taking in their surroundings, often give wyverns a menacing appearance.",
+    "This wyvern gains proficiency in Perception and Intimidation"
+  ],
+  [
+    "Avian Ancestry",
+    "Avaian features, such as feathers, may present themselves on mutated wyverns. They can range from full coverings to appearing only along joints and any amount in between. They vary in size, shape, and color, and physiology, so each avian mutation is uniquely different. These mutations result in lighter overall body structures, granting a more nimble frame that becomes more brittle, but much more agile in the process.",
+    "This wyvern -1 Constitution, +1 Dexterity, +10 Movement Speed"
+  ],
+  [
+    "Delta Wings",
+    "Variations in wing styles reflect the amount of drag and stability imposed on the wyvern's aerobatic features. This wyvern has Delta Wings with folds that carry down each side of the spine and through the tail, allowing them to be more stable in flight at higher speeds.",
+    "This wyvern +1 Dexterity, +10 Movement Speed"
+  ],
+  [
+    "Swept Wings",
+    "Variations in wing styles reflect the amount of drag and stability imposed on the wyvern's aerobatic features. This wyvern has Swept Wings, granting them a smaller set of stabilizer wings on their back legs, allowing them to be more agile in flight.",
+    "This wyvern +1 AC"
+  ],
+  [
+    "Adaptive Carapace",
+    "Sometimes, an incredible ability to manipulate appearance is present in some wyverns, with the skin beneath their scales having the ability to mottle its color and texture. This can be used to camouflage in to various environments such as mountain hills, grassy plains, or vast deserts.",
+    "This wyvern gains the ability to cast the 'Invisibility' spell once per day without expending a spell slot"
+  ],
+  [
+    "Adherent Grasp",
+    "Increased muscle strength and differeng talon tensile strengths can give a wyvern great ability to scale sheer faces, and even move up entire mountain stealthily by holding on to their faces without being detected.",
+    "This wyvern gains proficiency in Stealth, and a Climbing Speed equivalent to it's Walking Speed"
+  ],[
+    "Titanous Form",
+    "Increased metabolisim and muscle density in their youth can cause some wyverns to reach a titanous size by adulthood, often growing to twice as large as a normal wyvern. This, while it makes them slower in flight and less aerobatic, gives them incredible strength and an incredibly fortified constitution.",
+    "Benefits"
+  ],
+  [
+    "Title",
+    "Description of the mutation as it physically manifests.",
+    "Benefits"
+  ],
+]
