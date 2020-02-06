fc9790cedaa2e6d2f6ed31ed35d666a961b04bfb
GaryRidgway <gary-ridgway@uiowa.edu>
2020-02-05 20:57:34 -0600

    starting work on other stuff

---
+++ js/variables/WT_randomizer.js
@@ -0,0 +1,21 @@
+// This is the randomizer for Wyvern Type.
+function WT_randomize() {
+  // let wyvern_trait_text = "<div class='element-agnostic-mutations'>";
+
+
+  // for (i = 0; i < mutations.length; i++) {
+  //   let roll = Math.floor(Math.random() * 100);
+  //   if (roll <=10) {
+  //     let textblock = "<div class='mutation-" + i + "'>";
+  //     textblock += "<h2>" + mutations[i][0] + "</h2>";
+  //     textblock += "<p>" + mutations[i][1] + "</p>";
+  //     textblock += "<p>" + mutations[i][2] + "</p>";
+  //     textblock += "</div>";
+  //     wyvern_trait_text += textblock;
+  //   }
+  // }
+
+  // wyvern_trait_text += "</div>";
+
+  // return wyvern_trait_text;
+}
