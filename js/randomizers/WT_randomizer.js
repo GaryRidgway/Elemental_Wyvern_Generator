abc8229ee0cb41cfc49c60fb6d4bb5d270da013e
GaryRidgway <gary-ridgway@uiowa.edu>
2020-02-05 22:28:53 -0600

    syntax mistake

--- js/randomizers/WT_randomizer.js
+++ js/randomizers/WT_randomizer.js
@@ -2,7 +2,7 @@
 function WT_randomize() {
   let wyvern_type_text = "";
   let roll = Math.floor(Math.random() * wyvern_types.length);
-  let type = wyvern_types[type][0];
+  let type = wyvern_types[roll][0];
 
   let textblock = "<div class='wyvern-type-" + type + "'>";
   textblock += "<h2>" + type + " wyvern</h2>";
