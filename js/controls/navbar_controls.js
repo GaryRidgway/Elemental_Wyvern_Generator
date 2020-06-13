e8f36068954576f8b30938276a6354996970f99d
GaryRidgway <gary-ridgway@uiowa.edu>
2020-06-13 16:44:06 -0500

    saving wyvern stuff

--- js/controls/navbar_controls.js
+++ js/controls/navbar_controls.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
   save_save_modal.onclick = function () {
     let wyname = save_modal.querySelector('.modal-text-prompt').value
     let override = true;
-    if ( typeof saved_wyerns !== undefined) {
+    if ( typeof saved_wyerns !== "undefined") {
       if (saved_wyerns.includes(wyname)) {
         override = confirm('A wyvern called \"' + wyname + '\" already exists, are you sure want to overwrite it?');
       }
